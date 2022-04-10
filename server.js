216ab8528effcdede087276f4d2f6106644ed4d5
Ejdzrian <60348246+Ejdzrian@users.noreply.github.com>
2022-04-10 09:46:46 +0200

    zaktualizowany reaspons

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 9000;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Adrian jest super SUPER!');
+  res.end('Adrian jest super ekstra SUPER!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
